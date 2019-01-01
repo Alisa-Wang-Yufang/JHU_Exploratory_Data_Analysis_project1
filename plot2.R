0bc62929592dc68c01734e241d125e7394f805f0
Alisa-Wang-Yufang <40497106+Alisa-Wang-Yufang@users.noreply.github.com>
2019-01-01 22:53:04 +0800

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -3,9 +3,8 @@
 consumption$Date <- as.Date(consumption$Date, format = '%d/%m/%Y')
 consumption$DateTime <- strptime(paste(consumption$Date, consumption$Time),"%Y-%m-%d %H:%M:%S")
 # open device
-png(filename = './plot20.png', width = 480, height = 480, units='px')
+png(filename = './plot2.png', width = 480, height = 480, units='px')
 # plot figure
-Sys.setlocale(category = "LC_ALL", locale = "english")
 plot(consumption$DateTime, consumption$Global_active_power, xlab = '', ylab = 'Global Active Power (kilowatt)', type = 'l')
 # close device
 dev.off()
